8616430d50a73c0a192f60a69df50b97ea2068f8
Ultra03 <ultramlg1108@gmail.com>
2021-03-10 10:53:29 -0800

    Make Friends class into a singleton

--- src/main/java/com/evaan/frostburn/util/Friends.java
+++ src/main/java/com/evaan/frostburn/util/Friends.java
@@ -7,7 +7,29 @@ import java.util.ArrayList;
  * https://github.com/evaan
  */
 public class Friends {
-    public static ArrayList<String> friends = new ArrayList<>();
+    private static Friends instance = null;
 
-    public static boolean isFriend(String name) {return friends.contains(name);}
+    private ArrayList<String> friends;
+
+    private Friends() {
+        friends = new ArrayList<>();
+    }
+
+    public static Friends getInstance() {
+        if(instance == null)
+            instance = new Friends();
+        return instance;
+    }
+
+    public void addFriend(String name) {
+        friends.add(name);
+    }
+
+    public void removeFriend(String name) {
+        friends.remove(name);
+    }
+
+    public boolean isFriend(String name) {
+        return friends.contains(name);
+    }
 }
