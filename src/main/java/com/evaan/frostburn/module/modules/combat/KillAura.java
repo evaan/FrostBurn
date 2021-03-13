3949019bc9da0b717a84cd277903bdbcee6c4a96
majorsopa <73245063+majorsopa@users.noreply.github.com>
2021-03-12 19:07:02 -0500

    made killaura not crash
    when you were not within range of a player you would get an index error as there were no players, added a catch for that

--- src/main/java/com/evaan/frostburn/module/modules/combat/KillAura.java
+++ src/main/java/com/evaan/frostburn/module/modules/combat/KillAura.java
@@ -3,12 +3,10 @@ package com.evaan.frostburn.module.modules.combat;
 import com.evaan.frostburn.module.Module;
 import com.evaan.frostburn.util.Setting;
 import com.google.common.collect.Streams;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.SwordItem;
 import net.minecraft.util.Hand;
 
-import java.util.List;
 import java.util.stream.Collectors;
 
 /**
@@ -25,17 +23,21 @@ public class KillAura extends Module {
     public void onUpdate() {
         if (mc.player == null || mc.world == null) return;
         if(mc.player.getAttackCooldownProgress(0) < 1) return;
-        PlayerEntity player = (PlayerEntity) Streams.stream(mc.world.getEntities()).filter(e -> e instanceof PlayerEntity && mc.player.distanceTo(e) <= range.getValue() && e != mc.player).collect(Collectors.toList()).get(0);
-        if (player != null) {
-            System.out.println(player.getName());
-            if (switchItem.getValue()) {
-                for (int i = 0; i < 9; i++) {
-                    if (mc.player.inventory.getStack(i).getItem() instanceof SwordItem)
-                        mc.player.inventory.selectedSlot = i;
+        try {
+            PlayerEntity player = (PlayerEntity) Streams.stream(mc.world.getEntities()).filter(e -> e instanceof PlayerEntity && mc.player.distanceTo(e) <= range.getValue() && e != mc.player).collect(Collectors.toList()).get(0);
+            if (player != null) {
+                System.out.println(player.getName());
+                if (switchItem.getValue()) {
+                    for (int i = 0; i < 9; i++) {
+                        if (mc.player.inventory.getStack(i).getItem() instanceof SwordItem)
+                            mc.player.inventory.selectedSlot = i;
+                    }
                 }
+                mc.interactionManager.attackEntity(mc.player, player);
+                mc.player.swingHand(Hand.MAIN_HAND);
             }
-            mc.interactionManager.attackEntity(mc.player, player);
-            mc.player.swingHand(Hand.MAIN_HAND);
+        } catch (Exception e) {
+            System.out.println("killaura is kil");
         }
     }
 }
