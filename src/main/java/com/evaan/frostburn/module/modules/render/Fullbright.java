494c338245650e7c8d30cd153336afd30a6301d0
majorsopa <73245063+majorsopa@users.noreply.github.com>
2021-03-16 11:13:12 -0400

    Update Fullbright.java

--- src/main/java/com/evaan/frostburn/module/modules/render/Fullbright.java
+++ src/main/java/com/evaan/frostburn/module/modules/render/Fullbright.java
@@ -1,18 +1,18 @@
 package com.evaan.frostburn.module.modules.render;
 
 import com.evaan.frostburn.module.Module;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.world.World;
 
 public class Fullbright extends Module {
     public Fullbright() {super("Fullbright", Category.RENDER);}
 
+
     @Override
-    public void onUpdate() {
-
-        PlayerEntity player = mc.player;
-        World world = mc.world;
-
+    public void onEnable() {
         mc.options.gamma = 100;
+    }
+
+    @Override
+    public void onDisable() {
+        mc.options.gamma = 0;
     }
 }
